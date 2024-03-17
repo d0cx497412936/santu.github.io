73776855db540e2e0553e729188d9ee630517aaa
hemenghang <745935964@qq.com>
2024-03-17 13:07:56 +0800

    add listSort

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="panel-list">
         <div class="panel-list-menu" @click="goPage('/grid')">grid布局(pc&mobile)</div>
+        <div class="panel-list-menu" @click="goPage('/listSort')">拖拽排序列表</div>
         <NuxtLink to="/scrolledDemo" class="panel-list-menu">滚动条组件封装(pc&mobile)</NuxtLink>
         <div class="panel-list-menu" @click="goPage('/design')">PC可视化表单配置示范</div>
     </div>
 </template>
 <script setup lang="ts">
-const goPage = (path:string) => {
+const goPage = (path: string) => {
     navigateTo(path)
 }
 </script>
@@ -14,19 +15,5 @@ const goPage = (path:string) => {
 <style lang="scss">
 .panel-list {
     padding: 16px;
-    text-align: center;
-    &-menu {
-        
-        margin: auto auto 20px auto;
-        border: 1px solid #dcdcdc;
-        border-radius: 6px;
-        line-height: 40px;
-        text-align: center;
-        display: block;
-        max-width: 500px;
-        color: #111;
-        text-decoration: none;
-        cursor: pointer;
-    }
 }
 </style>
---
+++ pages/listSort.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="panel-list">
+        <draggable v-model="list"  transition-group="500" animation="100" :sort="true">
+            <template #item="{ element,index }">
+                <div class="panel-list-menu" :key="element.id">
+                    {{ element.title }}
+                </div>
+            </template>
+        </draggable>
+
+    </div>
+</template>
+<script setup lang="ts">
+import draggable from "vuedraggable";
+const list:any= ref([])
+for (let i = 1; i < 10; i++) {
+    list.value.push({
+        id: 'sort' + i,
+        title:'序号：'+i
+    })
+}
+</script>
