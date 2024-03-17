cebc898c8ef9d1d3221a329f3725783954dfbf10
hemenghang <745935964@qq.com>
2024-03-17 15:31:52 +0800

    update add charts

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
     <div class="panel-list">
         <div class="panel-list-menu" @click="goPage('/grid')">grid布局(pc&mobile)</div>
         <div class="panel-list-menu" @click="goPage('/listSort')">拖拽排序列表</div>
+        <div class="panel-list-menu" @click="goPage('/chartsDemo')">数据表格</div>
         <NuxtLink to="/scrolledDemo" class="panel-list-menu">滚动条组件封装(pc&mobile)</NuxtLink>
         <div class="panel-list-menu" @click="goPage('/design')">PC可视化表单配置示范</div>
     </div>
